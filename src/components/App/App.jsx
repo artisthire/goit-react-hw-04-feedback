f25f4a7c4b99dde47995e712abbb262cdd6bcfee
Andrey <artist_hire@ukr.net>
2022-06-15 15:21:46 +0300

    Add hook in App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,32 +1,44 @@
-import { Component } from 'react';
+import { useReducer } from 'react';
 import { Wrapper, Container } from './App.styled';
+
 import Section from 'components/Section';
 import FeedbackOptions from 'components/FeedbackOptions';
 import Statistics from 'components/Statistics';
 import Notification from 'components/Notification';
 
-class App extends Component {
-  state = {
+function reducer(state, action) {
+  switch (action.type) {
+    case 'good':
+      return { ...state, good: state.good + action.payload };
+    case 'neutral':
+      return { ...state, neutral: state.neutral + action.payload };
+    case 'bad':
+      return { ...state, bad: state.bad + action.payload };
+    default:
+      throw new Error('Unexpected type');
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, {
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  handleLeaveFeedback = feedbackName => {
+  function handleLeaveFeedback(feedbackName) {
     return () => {
-      this.setState(prevState => ({
-        [feedbackName]: prevState[feedbackName] + 1,
-      }));
+      dispatch({ type: feedbackName, payload: 1 });
     };
-  };
-
-  countTotalFeedback() {
-    return Object.values(this.state).reduce((summ, value) => summ + value, 0);
   }
 
-  countPositiveFeedbackPercentage() {
-    const totalFeedback = this.countTotalFeedback();
-    const positiveFeedbackCount = Object.entries(this.state).reduce(
+  function countTotalFeedback() {
+    return Object.values(state).reduce((summ, value) => summ + value, 0);
+  }
+
+  function countPositiveFeedbackPercentage() {
+    const totalFeedback = countTotalFeedback();
+    const positiveFeedbackCount = Object.entries(state).reduce(
       (summ, [feedbackName, value]) =>
         feedbackName === 'good' ? summ + value : summ,
       0
@@ -40,36 +52,34 @@ class App extends Component {
     return percentage + '%';
   }
 
-  render() {
-    const totalFeedback = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-    const feedbackBtns = Object.keys(this.state);
-    const statisticData = Object.entries(this.state);
+  const totalFeedback = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+  const btnNames = Object.keys(state);
+  const statisticData = Object.entries(state);
 
-    return (
-      <Wrapper>
-        <Container>
-          <Section title="Please leave feedback">
-            <FeedbackOptions
-              options={feedbackBtns}
-              onLeaveFeedback={this.handleLeaveFeedback}
+  return (
+    <Wrapper>
+      <Container>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={btnNames}
+            onLeaveFeedback={handleLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {totalFeedback > 0 && (
+            <Statistics
+              options={statisticData}
+              total={totalFeedback}
+              positivePercentage={positivePercentage}
             />
-          </Section>
-          <Section title="Statistics">
-            {totalFeedback > 0 && (
-              <Statistics
-                options={statisticData}
-                total={totalFeedback}
-                positivePercentage={positivePercentage}
-              />
-            )}
+          )}
 
-            {!totalFeedback && <Notification message="There is no feedback" />}
-          </Section>
-        </Container>
-      </Wrapper>
-    );
-  }
+          {!totalFeedback && <Notification message="There is no feedback" />}
+        </Section>
+      </Container>
+    </Wrapper>
+  );
 }
 
 export default App;
